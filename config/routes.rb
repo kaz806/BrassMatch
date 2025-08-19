0071086cc9f5a6ee41e4b72495ef6a7007e37dee
kaz806 <s11502301306@toyo.jp>
2025-08-20 03:08:38 +0900

    （変更内容の説明）

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tweets
-  resources :perfumes, only: [:index, :new, :create]
+  resources :perfumes, only: [:index, :new, :show, :create]
  
 
   get 'post/:step', to: 'posts#show', as: 'post'
